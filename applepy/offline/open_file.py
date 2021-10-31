12d39a43343454b168e7fc9b0796354e38bc1837
Mateusz Suchocki <mateuszsuchocki7@gmail.com>
2021-10-31 10:39:45 -0700

    Handle exceptions

--- applepy/offline/open_file.py
+++ applepy/offline/open_file.py
@@ -1,25 +1,37 @@
 import click
+from click.exceptions import ClickException
 from scapy.all import sniff
+from scapy.error import Scapy_Exception
 
 
-@click.command()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('-i', '--index', help="Packet's index counting from 0. If not specified then displaying all the packets", type=int)
 @click.option('-f', '--filter', help='Applies filter to displayed packets', type=str)
 @click.argument('file', type=click.File('rb'))
-def open(index, file, filter):
-    """Displays the given file"""
+def open_pcap(index, file, filter):
+    """
+    Displays a packet capture file
 
-    # TODO: Check file extensions, index, filter and hundreds of other things
+    FILE is a packet capture file to be displayed
+    """
 
-    packets = sniff(offline=file, filter=filter)
+    try:
+        # Loads packet capture file
+        packets = sniff(offline=file, filter=filter)
 
-    if index is None:
-        click.echo(packets.nsummary())
-    else:
-        if index < 0 or index >= len(packets):
-            click.echo(
-                f'There is no packet with the given index. Index range 0-{len(packets)-1} ')
-            exit(0)
+        if index is None:
+            click.echo(packets.nsummary())
+        else:
+            if index < 0 or index >= len(packets):
+                raise ClickException(
+                    f'There is no packet with the given index. Index range 0-{len(packets)-1}')
 
-        click.echo(f'Packet #{str(index)}')
-        click.echo(packets[index].show())
+            click.echo(f'Packet #{str(index)}')
+            click.echo(packets[index].show())
+
+    except Scapy_Exception as e:
+        click.echo(e)
+        exit(1)
