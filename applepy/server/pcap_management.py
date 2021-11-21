ec781f17361e797a9239896a75917934378c0922
Mateusz Suchocki <mateuszsuchocki7@gmail.com>
2021-11-21 12:21:15 -0800

    Add cmd execution, log listing and log download

--- applepy/server/pcap_management.py
+++ applepy/server/pcap_management.py
@@ -1,4 +1,5 @@
 import time
+import subprocess
 
 from ifcfg import interfaces
 from typing import Optional
@@ -7,6 +8,7 @@ from fastapi.responses import FileResponse
 from scapy.all import sniff, wrpcap
 from pydantic import BaseModel
 from os import listdir
+from os.path import isfile, join
 
 app = FastAPI()
 
@@ -17,9 +19,17 @@ class CaptureConfig(BaseModel):
     filter: Optional[str] = None
 
 
+class Command(BaseModel):
+    cmd: str
+    args: Optional[str] = None
+
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+# PCAP
+################################################
 
 
 @app.get("/network-config")
@@ -52,3 +62,34 @@ def sniff_packets(iface, count, filter):
     pcap = sniff(count=count, iface=iface, filter=filter)
     wrpcap(f'pcaps/packet_capture_{str(time.time()).replace(".", "_")}', pcap)
     return
+
+# LOG
+################################################
+
+
+@app.get('/logs')
+async def get_log_files():
+    log_path = '/var/log/'
+    log_files = [f for f in listdir(log_path) if isfile(join(log_path, f))]
+    return {'log_files': log_files}
+
+
+@app.get('/logs/{file_id}', response_class=FileResponse)
+async def get_log_file(file_id: int):
+    log_path = '/var/log/'
+    log_files = [f for f in listdir(log_path) if isfile(join(log_path, f))]
+    log_to_download = log_path + log_files[file_id]
+    return log_to_download
+
+# CMD
+################################################
+
+
+@app.post('/cmd')
+async def run_command(cmd: Command):
+    full_command = []
+    full_command.append(cmd.cmd)
+    if cmd.args is not None:
+        full_command.extend(cmd.args.split(' '))
+    result = subprocess.run(full_command, capture_output=True)
+    return {'result': result}
--- applepy/server/tests/req.py
+++ applepy/server/tests/req.py
@@ -6,10 +6,17 @@ url = 'http://127.0.0.1:8000/'
 # r = requests.get(url + 'network-config')
 
 # POST /packet-capture
-r = requests.post(url + 'packet-capture', json={
-    'count': 50,
-    'iface': 'ens33',
-    'filter': ''
+# r = requests.post(url + 'packet-capture', json={
+#     'count': 50,
+#     'iface': 'ens33',
+#     'filter': ''
+# })
+
+# POST /cmd
+r = requests.post(url + 'cmd', json={
+    'cmd': 'ls',
+    'args': '-l ../..'
 })
 
-print(r.text)
+
+print(r.json())
