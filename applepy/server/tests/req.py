118e59a8810b67800123ccaa59db8461139c28d1
Mateusz Suchocki <mateuszsuchocki7@gmail.com>
2021-11-21 10:02:52 -0800

    Update req.py

--- applepy/server/tests/req.py
+++ applepy/server/tests/req.py
@@ -2,7 +2,10 @@ import requests
 
 url = 'http://127.0.0.1:8000/'
 
+# GET /network-config
 # r = requests.get(url + 'network-config')
+
+# POST /packet-capture
 r = requests.post(url + 'packet-capture', json={
     'count': 10,
     'iface': 'ens33',
